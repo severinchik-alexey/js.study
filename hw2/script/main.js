91ba714abae8b7acfa3d81c30d6de2b3f0097263
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-25 01:46:57 +0300

    add function

--- hw2/script/main.js
+++ hw2/script/main.js
@@ -1,26 +1,7 @@
-/*var equation = (a, b, c) => {
-    var d = (Math.pow(b,2)) - (4*a*c);
-    if (d < 0) {
-        alert(`Корней нет`);
-        return undefined;
-    } else if (d == 0) {
-        var x = (-b / (2 * a));
-        alert(`x равен ${x}`);
-        return x;
-    } else if (d > 0) {
-        var x1 = ((-b + Math.sqrt(d)) / (2 * a));
-        var x2 = ((-b - Math.sqrt(d)) / (2 * a));
-        alert(`x1 равен ${x1} x2 равен ${x2}`);
-        return x1, x2;
-    }
-};
-alert(equation(1,-6,9));*/
-
-
-
 let a = prompt('Введите a');
 let b = prompt('Введите b');
 let c = prompt('Введите c');
+const disk = () => console.log(-b / (2 * a));
 function solveEquation(a, b, c) {
     let d = b ** 2 - 4 * a * c;
     let x;
@@ -28,9 +9,9 @@ function solveEquation(a, b, c) {
         alert('Корней нет');
         return undefined;
     } else if (d === 0) {
-        x = (-b / (2 * a));
+        x = disk;
         alert(`x равен ${x}`);
-        return x;
+        return disk;
     } else if (d > 0) {
         let x1 = ((-b + d ** 0.5) / (2 * a));
         let x2 = ((-b - d ** 0.5) / (2 * a));
