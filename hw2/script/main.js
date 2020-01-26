be6e66773cdc8f86b1cb98613612b00624846d7a
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-26 20:39:50 +0300

    minor fix

--- hw2/script/main.js
+++ hw2/script/main.js
@@ -1,18 +1,20 @@
-let a = prompt('Введите a');
-let b = prompt('Введите b');
-let c = prompt('Введите c');
-let d = (b * b) - (4 * a * c);
-// const disk = () => console.log(-b / (2 * a)); 
 function solveEquation() {
+    let a = prompt('Введите a');
+    let b = prompt('Введите b');
+    let c = prompt('Введите c');
+    let d = (b ** 2) - (4 * a * c);
     if (d < 0) {
         alert('Корней нет');
+        return;
     } else if (d === 0) {
         let x = -b / (2 * a);
         alert(`x равен ${x}`);
+        return x;
     } else if (d > 0) {
-        let x1 = ((-b + d ** 0.5) / (2 * a));
-        let x2 = ((-b - d ** 0.5) / (2 * a));
-        alert(`x1 = ${ x1 } \nx2 = ${ x2 }`);
+        let x1 = (-b + d ** 0.5) / (2 * a);
+        let x2 = (-b - d ** 0.5) / (2 * a);
+        alert(`x1 = ${x1} \nx2 = ${x2}`);
+        return [x1, x2];
     }
 }
-console.Log(solveEquation());
+console.log(solveEquation());
