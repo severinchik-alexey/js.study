321075099a5c5ae6d2cc44181d073e206b6693a8
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-27 19:10:24 +0300

    Update main.js

--- hw2/script/main.js
+++ hw2/script/main.js
@@ -1,15 +1,15 @@
 function solveEquation() {
     let a;
     do {
-        a = Number(prompt('Введите a'));
+        a = parseInt(prompt('Введите a'));
     } while (isNaN(a) || !a || a===0);
     let b;
     do {
-        b = Number(prompt('Введите b'));
+        b = parseInt(prompt('Введите b'));
     } while (isNaN(b) || !b);
     let c;
     do {
-        c = Number(prompt('Введите c'));
+        c = parseInt(prompt('Введите c'));
     } while (isNaN(c) || !c);
 
     let d = (b ** 2) - (4 * a * c);
@@ -24,7 +24,7 @@ function solveEquation() {
         let x1 = (-b + d ** 0.5) / (2 * a);
         let x2 = (-b - d ** 0.5) / (2 * a);
         alert(`x1 = ${x1} \nx2 = ${x2}`);
-        return Number([x1, x2]);
+        return parseInt([x1, x2]);
     }
 }
 console.log(solveEquation());
