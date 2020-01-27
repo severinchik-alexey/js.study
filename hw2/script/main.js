6c5ea6193129848e4c96ccda74d639a370f8a7d2
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-27 19:25:45 +0300

    Update main.js

--- hw2/script/main.js
+++ hw2/script/main.js
@@ -14,16 +14,13 @@ function solveEquation() {
 
     let d = (b ** 2) - (4 * a * c);
     if (d < 0) {
-        alert('Корней нет');
         return [];
     } else if (d === 0) {
         let x = -b / (2 * a);
-        alert(`x равен ${x}`);
         return parseInt[x];
     } else if (d > 0) {
         let x1 = (-b + d ** 0.5) / (2 * a);
         let x2 = (-b - d ** 0.5) / (2 * a);
-        alert(`x1 = ${x1} \nx2 = ${x2}`);
         return parseInt([x1, x2]);
     }
 }
