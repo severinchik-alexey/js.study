b4bfb150dd720a955dab6aa451473c7871024df2
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-27 03:23:31 +0300

    create new hw

---
+++ hw3/script/main.js
@@ -0,0 +1,23 @@
+// let array = [18, 2, 23, 6, 32, 10, 19];
+// let max = maxV(array);
+// alert(max);
+// function maxV(array) {
+//     let max = array[0];
+//     for (let i = 0; i < array.length; i++) {
+//         if (max < array[i]) max = array[i];
+//     }
+//     return max;
+// };
+// console.log(maxV(array));
+
+let array = [18, 2, 23, 6, 32, 10, 19];
+let max = maxV(array);
+alert(max);
+function maxV(array) {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if ((max - array[i]) < 0) max = array[i];
+    }
+    return max;
+};
+console.log(maxV(array));
