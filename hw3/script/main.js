91957e30692270e7c7ded1a193ef9c2219151149
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-27 19:47:34 +0300

    Update main.js

--- hw3/script/main.js
+++ hw3/script/main.js
@@ -24,11 +24,11 @@ function maxV(array) {
 };
 alert(max);
 
-function maxV(...arguments) {
-    let max = arguments[0];
-    for (let i = 0; i < arguments.length; i++) {
-        if ((max - arguments[i]) < 0) {
-            max = arguments[i];
+function maxV(...array) {
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if ((max - array[i]) < 0) {
+            max = array[i];
         }
     }
     return max;
