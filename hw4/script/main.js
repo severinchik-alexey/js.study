5aa199fd886eaeb31ae89b69d69cff65fc8d7cc5
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-29 16:51:30 +0300

    fix cycle

--- hw4/script/main.js
+++ hw4/script/main.js
@@ -1,16 +1,27 @@
 function treeSum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === 'object') {
-            sum += treeSum(arr[i]);
-        } else {
-            sum += arr[i];
-        }
+        sum += Array.isArray(arr[i]) ? treeSum(arr[i]) : arr[i];
     }
     return sum;
 }
-console.log(treeSum([ 5, 7, 
-        [ 4, [2], 8, [1,3], 2 ], 
-        [ 9, [] ], 
-        1, 8, 10
-      ]));
+console.log(treeSum([5, 7,
+    [4, [2], 8, [1, 3], 2],
+    [9, []],
+    1, 8, 10
+]));
+
+/*
+function treeSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].splice ? treeSum(arr[i]) : arr[i];
+    }
+    return sum;
+}
+console.log(treeSum([5, 7,
+    [4, [2], 8, [1, 3], 2],
+    [9, []],
+    1, 8, 10
+]));
+*/
