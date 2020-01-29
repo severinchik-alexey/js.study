25d3aec623255b6e771dae69a5a64bd4e01419da
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-29 19:18:18 +0300

    Update main.js

--- hw4/script/main.js
+++ hw4/script/main.js
@@ -1,7 +1,10 @@
 function treeSum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        sum += Array.isArray(arr[i]) ? treeSum(arr[i]) : arr[i];
+        if (Array.isArray(arr[i])) {
+            sum += treeSum(arr[i]);
+        } else {
+            sum += arr[i];    
     }
     return sum;
 }
@@ -10,6 +13,20 @@ console.log(treeSum([5, 7,
     [9, []],
     1, 8, 10
 ]));
+
+/*
+function treeSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += Array.isArray(arr[i]) ? treeSum(arr[i]) : arr[i];
+    }
+    return sum;
+}
+console.log(treeSum([5, 7,
+    [4, [2], 8, [1, 3], 2],
+    [9, []],
+    1, 8, 10
+]));*/
 
 /*
 function treeSum(arr) {
