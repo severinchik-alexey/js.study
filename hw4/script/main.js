1b6862a0114145c7233046353026799323d65d3d
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-01-29 14:13:11 +0300

    update function

--- hw4/script/main.js
+++ hw4/script/main.js
@@ -1,12 +1,12 @@
-function func(arr) {
+function treeSum(arr) {
     let sum = 0;
     for (let value of arr) {
         if (isNaN(value)) {
-            sum += func(value);
+            sum += treeSum(value);
         } else {
             sum += value;
         }
     }
     return sum;
 }
-console.log(func([5, 2, [1, 4, [5, [4, [5, 8, 9], 7], 11], 7], 8, [9, 10]]));
+console.log(treeSum([5, 2, [1, 4, [5, [4, [5, 8, 9], 7], 11], 7], 8, [9, 10]]));
