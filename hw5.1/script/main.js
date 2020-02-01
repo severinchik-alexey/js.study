8d31700d23233c9d3e29ae581c19edbfb98883de
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-01 10:35:05 +0300

    Update main.js

--- hw5.1/script/main.js
+++ hw5.1/script/main.js
@@ -1,7 +1,7 @@
 function objectsAreEqual(a, b) {
     for (let i in a) {
-        if (a.hasOwnProperty(i)) {
-            if (b.hasOwnProperty(i)) {
+        if (a[i]) {
+            if (b[i]) {
                 if (typeof a[i] === 'object') {
                     if (!objectsAreEqual(a[i], b[i])) return false;
                 } else {
