144359f4bd6f93df2e04cc5053f3cc494b5fcc65
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-03 19:21:19 +0300

    Update main.js

--- hw6/script/main.js
+++ hw6/script/main.js
@@ -11,7 +11,7 @@ let calculator = (function () {
       result -= x;
     },
     multiply(x) {
-      result *=x; 
+      result *= x; 
     },
     divide(x) {
       result /= x;
@@ -20,7 +20,7 @@ let calculator = (function () {
       result **= x;
     },
     percent(x) {
-      result %=x;
+      result %= x;
     },
     sqrt(x) {
       result = Math.sqrt(x);
