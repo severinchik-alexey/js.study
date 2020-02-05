06bc56875bdb38ff76a521b6e509143831f38319
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-05 16:15:33 +0300

    create hw7

---
+++ hw7/main.js
@@ -0,0 +1,18 @@
+const user = {
+    name: 'Alexey',
+    sex: 'male'
+};
+
+function info(surname, age, ) {
+    console.log(`Name is ${this.name}, Surname is ${surname}, age is ${age}, sex is ${this.sex}`);
+}
+
+function myBind(func, context, ...args) {
+    return function (...arg) {
+        return func.call(context, ...args.concat(arg))
+    }
+}
+
+myBind(info, user)('Severinchik', '23')
+myBind(info, user, 'Severinchik')('23')
+myBind(info, user, 'Severinchik', '23')()
