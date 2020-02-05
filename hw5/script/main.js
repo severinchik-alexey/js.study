a682017f1a624c1c1e6ca1be7b4f5209c77294d5
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-05 19:14:04 +0300

    Update main.js

--- hw5/script/main.js
+++ hw5/script/main.js
@@ -4,9 +4,7 @@ function unique(arr) {
     for (let i = 0; i < arr.length; i++) {
         b[arr[i]] = arr[i];
     }
-    for (let i in b) {
-        result.push(b[i]);
-    }
+    for (let i in b) result.push(b[i]);
     return result;
 };
 console.log(unique([2,3,4,5,22,4,2,7,8,3,1,2,'nan','nen','nan']));
