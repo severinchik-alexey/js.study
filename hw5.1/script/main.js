aaf8284b3567f5e8d92e519e7766800804df771a
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-05 16:29:11 +0300

    minor fix

--- hw5.1/script/main.js
+++ hw5.1/script/main.js
@@ -1,17 +1,14 @@
 function objectsAreEqual(a, b) {
     for (let i in a) {
-        if (a[i]) {
-            if (b[i]) {
-                if (typeof a[i] === 'object') {
-                    if (!objectsAreEqual(a[i], b[i])) return false;
-                } else {
-                    if (a[i] !== b[i]) return false;
-                }
+        if (a[i] && b[i]) {
+            if (typeof a[i] === 'object') {
+                if (!objectsAreEqual(a[i], b[i])) return false;
             } else {
-                return false;
+                if (a[i] !== b[i]) return false;
             }
+        } else {
+            return false;
         }
-    }
-    return true;
+    } return true;
 }
 console.log(objectsAreEqual({ a: 7 }, { b: 9 }));
