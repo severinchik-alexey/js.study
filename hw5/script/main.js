c7b9f9bf8125234ccbf7a349af19ed5c366e91b7
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-07 16:51:03 +0300

    Update main.js

--- hw5/script/main.js
+++ hw5/script/main.js
@@ -1,10 +1,9 @@
 function unique(arr) {
-    let result = [];
-    let b = {};
-    for (let i = 0; i < arr.length; i++) {
-        b[arr[i]] = arr[i];
-    }
-    for (let i in b) result.push(b[i]);
-    return result;
-};
+        let b = {};
+        for (let i = 0; i < arr.length; i++) {
+            b[arr[i]] = arr[i];
+        }
+
+        return Object.keys(b);
+    };
 console.log(unique([2,3,4,5,22,4,2,7,8,3,1,2,'nan','nen','nan']));
