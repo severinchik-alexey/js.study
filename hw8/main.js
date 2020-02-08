7f8c809282da776126804cb3e1942c8d43dcb041
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-08 03:28:12 +0300

    create hw8

---
+++ hw8/main.js
@@ -0,0 +1,27 @@
+class HashStorageFunc {
+    constructor() {
+        this.addValue = function (key, value) {
+            this[key] = value;
+        };
+        this.getValue = function (key) {
+            return (`
+                Напиток: ${key}
+                Алкогольный: ${this[key]}`);
+        };
+        this.deleteValue = function (key) {
+            if (this[key]) {
+                delete this[key];
+                return true;
+            }
+            return false;
+        };
+        this.getKeys = function () {
+            return Object.keys(this);
+        };
+    }
+}
+let drinkStorage = new HashStorageFunc();
+
+drinkStorage.addValue('Пина колада', 'да');
+drinkStorage.addValue('Сок', 'нет');
+drinkStorage.deleteValue('Сок');
