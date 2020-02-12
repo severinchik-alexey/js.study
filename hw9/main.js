0dcbdef6ec5eba2a90ce1f839ce8775a49a602be
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-12 17:00:03 +0300

    Update main.js

--- hw9/main.js
+++ hw9/main.js
@@ -1,40 +1,56 @@
 function HashStorageFunc() {
-    this._hash = {};
     this.addValue = function (key, value) {
-        this._hash[key] = value;
+        this[key] = value;
     };
     this.getValue = function (key) {
-        return this._hash[key];
+        return (`
+		Напиток: ${key}
+		Алкогольный: ${this[key]}
+    `);
     };
     this.deleteValue = function (key) {
-        return this._hash[key] ? delete this._hash[key] : false;
+        return this[key] ? delete this[key] : false;
     };
     this.getKeys = function () {
-        return Object.keys(this._hash);
+        return Object.keys(this);
     };
 }
 
-function ClassA() {
-    HashStorageFunc.call(this);
-    this.deleteValue = function (key) {
-        if (key in this._hash) {
-            delete this._hash[key];
-            return 'Successfully deleted';
-        }
-        return 'No such value';
-    };
-}
+let drinkStorage = new HashStorageFunc();
 
-function ClassB() {
-    HashStorageFunc.call(this);
-    this.getEntries = function() {
-        return Object.entries(this._hash);
-    }
-}
-let classA = new ClassA();
-let classB = new ClassB();
-
-drinkStorage.addValue('Пина колада', {'Алкогль' : 'да'});
-drinkStorage.addValue('Сок', {'Алкоголь':'нет'});
+drinkStorage.addValue('Пина колада', 'да' );
+drinkStorage.addValue('Сок',  'нет' );
 drinkStorage.deleteValue('Сок');
 console.log(drinkStorage.getValue('Пина колада'));
+
+
+function ClassA(useful) {
+    HashStorageFunc.call(this);
+    this.getUseful = function () {
+        return (`
+        Полезный: ${useful}
+        `);
+    };
+}
+
+let drink = new ClassA('Да');
+
+drink.addValue('Пина колада', 'да');
+drink.addValue('Сок', 'да');
+drink.deleteValue('Сок');
+drink.getValue('Пина колада');
+drink.getUseful();
+
+
+function ClassB(drunk) {
+    HashStorageFunc.call(this, arguments);
+    let parentGetDrink = this.getValue;
+    this.getValue = function (key) {
+        parentGetDrink.apply(this, arguments);
+        if (this[key]) {
+            console.log(`${key} это знаение есть в списке`);
+        } else console.log('этого значения не сущетвует в списке');
+    };
+}
+
+const drunk = new ClassB('Напиток');
