70739565741731d551172cca97bb3631dfa03b67
Alexey Severinchik <58680020+severinchik-alexey@users.noreply.github.com>
2020-02-29 08:54:33 +0300

    commit to master

---
+++ clock_canvas/main.js
@@ -0,0 +1,80 @@
+let radius = 220;
+let angle = 30; 
+let yellowR = 300; 
+let greenR = 40;
+let secLength = 290;
+let minLength = 180;
+let hourLength = 150;
+const pi = Math.PI;
+const sin = Math.sin;
+const cos = Math.cos;
+
+let clock = document.getElementById("clock");
+let ctx = clock.getContext('2d');
+
+function drawClock() {
+    ctx.beginPath();
+    ctx.arc(300, 300, 300, 0, 2 * pi);
+    ctx.fillStyle = "#fcca66";
+    ctx.fill();
+
+    for (let i = 1; i <= 12; i++) {
+        let angleRad = angle * i * 2 * pi / 360;
+        let greenCenterX = yellowR + radius * sin(angleRad);
+        let greenCenterY = yellowR - radius * cos(angleRad);
+        ctx.beginPath();
+        ctx.arc(greenCenterX, greenCenterY, greenR, 0, 2 * pi);
+        ctx.fillStyle = "#48b382";
+        ctx.fill();
+
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.font = "normal bold 40px Arial";
+        ctx.fillText(i, greenCenterX, greenCenterY);
+    }
+
+    let now = new Date;
+    let sec = now.getSeconds() * 6 - 90;
+    let min = now.getMinutes() * 6 - 90; 
+    let hour = (now.getHours() + now.getMinutes() / 60 + now.getSeconds() / 3600) * 30 - 90;
+    let secX, minX, hourX;
+    let secY, minY, hourY;
+
+    let secRad = sec * 2 * pi / 360;
+    let minRad = min * 2 * pi / 360;
+    let hourRad = hour * 2 * pi / 360;
+
+    secX = yellowR + secLength * cos(secRad);
+    secY = yellowR + secLength * sin(secRad);
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.lineCap = 'round';
+    ctx.moveTo(300, 300);
+    ctx.lineTo(secX, secY);
+    ctx.stroke();
+
+    minX = yellowR + minLength * cos(minRad);
+    minY = yellowR + minLength * sin(minRad);
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 4;
+    ctx.moveTo(300, 300);
+    ctx.lineTo(minX, minY);
+    ctx.stroke();
+
+    hourX = yellowR + hourLength * cos(hourRad);
+    hourY = yellowR + hourLength * sin(hourRad);
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 8;
+    ctx.moveTo(300, 300);
+    ctx.lineTo(hourX, hourY);
+    ctx.stroke();
+}
+
+setInterval(drawClock, 40);
--- clock_svg/main.js
+++ clock_svg/main.js
@@ -1,131 +1,109 @@
-let clockSVG = document.getElementById("clock");
-clockSVG.style.position = "relative";
-let clock = document.createElementNS("http://www.w3.org/2000/svg",'circle');
-clock.setAttribute("stroke","green");
-clock.setAttribute("fill","gold");
-clock.setAttribute("r",200);
-clock.setAttribute("cx",400);
-clock.setAttribute("cy",300);
-clockSVG.append(clock);
+const svgNS = "http://www.w3.org/2000/svg";
+const svg = document.getElementById("svg");
+const w = parseFloat(svg.getAttribute("width"));
+const h = parseFloat(svg.getAttribute("height"));
+const yellowR = w / 2;
+const radius = 0.8 * yellowR;
+const pi = Math.PI;
+const cos = Math.cos;
+const sin = Math.sin;
 
-for (let i = 1; i <= 12; i++) {
-  let digit = document.createElementNS("http://www.w3.org/2000/svg",'circle');
-  clockSVG.append(digit);
-  digit.append(i);
-  digit.setAttribute("stroke","green");
-  digit.setAttribute("fill","yellow");
-  digit.setAttribute("r",20);
-  digit.style.position = "absolute";
-  
-  switch (i) {
-    case 1:
-        digit.setAttribute("cx",470);
-        digit.setAttribute("cy",170);
-      break;
+function yellowCircle() {
+  let yellow = document.createElementNS(svgNS, "circle"); //создаем область циферблата
+  yellow.setAttribute("cx", w / 2);
+  yellow.setAttribute("cy", h / 2);
+  yellow.setAttribute("r", w / 2);
+  yellow.setAttribute("fill", "#fcca66");
+  yellow.setAttribute("stroke", "none");
 
-    case 2:
-        digit.setAttribute("cx",530);
-        digit.setAttribute("cy",230);
-      break;
+  svg.appendChild(yellow);
+}
+yellowCircle();
+function greenCircle() {
+  // создаем зеленые кружки
+  let angle = 30;
+  for (i = 1; i <= 12; i++) {
+    let green = document.createElementNS(svgNS, "circle");
+    svg.appendChild(green);
+    let angleRad = (angle * i * pi) / 180;
 
-    case 3:
-        digit.setAttribute("cx",560);
-        digit.setAttribute("cy",300);
-      break;
+    let greenCenterX = yellowR + radius * sin(angleRad);
+    let greenCenterY = yellowR - radius * cos(angleRad);
 
-    case 4:
-        digit.setAttribute("cx",530);
-        digit.setAttribute("cy",370);
-      break;
-
-    case 5:
-        digit.setAttribute("cx",470);
-        digit.setAttribute("cy",430);
-      break;
-
-    case 6:
-        digit.setAttribute("cx",400);
-        digit.setAttribute("cy",460);
-      break;
-
-    case 7:
-        digit.setAttribute("cx",330);
-        digit.setAttribute("cy",430);
-      break;
-
-    case 8:
-        digit.setAttribute("cx",270);
-        digit.setAttribute("cy",370);
-      break;
-
-    case 9:
-        digit.setAttribute("cx",240);
-        digit.setAttribute("cy",300);
-      break;
-
-    case 10:
-        digit.setAttribute("cx",270);
-        digit.setAttribute("cy",230);
-      break;
-
-    case 11:
-        digit.setAttribute("cx",330);
-        digit.setAttribute("cy",170);
-      break;
-
-    case 12:
-        digit.setAttribute("cx",400);
-        digit.setAttribute("cy",140);
-      break;
+    green.setAttribute("cx", greenCenterX);
+    green.setAttribute("cy", greenCenterY);
+    green.setAttribute("r", 45);
+    green.setAttribute("fill", "#48b382");
   }
 }
-let hour = document.createElementNS("http://www.w3.org/2000/svg",'line');
-hour.setAttribute("x1",400);
-hour.setAttribute("y1",300);
-hour.setAttribute("x2",400);
-hour.setAttribute("y2",180);
-hour.setAttribute("stroke","black");
-hour.setAttribute("stroke-width","20");
-clockSVG.append(hour);
+greenCircle();
 
-let minute = document.createElementNS("http://www.w3.org/2000/svg",'line');
-minute.setAttribute("x1",400);
-minute.setAttribute("y1",300);
-minute.setAttribute("x2",400);
-minute.setAttribute("y2",140);
-minute.setAttribute("stroke","black");
-minute.setAttribute("stroke-width","10");
-clockSVG.append(minute);
-
-let secunde = document.createElementNS("http://www.w3.org/2000/svg",'line');
-secunde.setAttribute("x1",400);
-secunde.setAttribute("y1",300);
-secunde.setAttribute("x2",400);
-secunde.setAttribute("y2",120);
-secunde.setAttribute("stroke","red");
-secunde.setAttribute("stroke-width","5");
-clockSVG.append(secunde);
-
-let boltik = document.createElementNS("http://www.w3.org/2000/svg",'circle');
-boltik.setAttribute("stroke","green");
-boltik.setAttribute("fill","gold");
-boltik.setAttribute("r",15);
-boltik.setAttribute("cx",400);
-boltik.setAttribute("cy",300);
-clockSVG.append(boltik);
-
-clockRefresh()
-function clockRefresh() {
-  const date = new Date();
-  const seconds = date.getSeconds();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  secunde.style.cssText = "transform-origin: 57% 43%;"; 
-  minute.style.cssText = "transform-origin: 57% 43%;"; 
-  hour.style.cssText = "transform-origin: 57% 43%;"; 
-  secunde.style.transform = `rotate(${seconds * 6}deg)`;
-  minute.style.transform = `rotate(${minutes * 6}deg)`;
-  hour.style.transform = `rotate(${hours * 30 + minutes / 2}deg)`;
+function numbers() {
+  //рисуем цифры на циферблате
+  let angle = 30;
+  for (i = 1; i <= 12; i++) {
+    let text = document.createElementNS(svgNS, "text");
+    svg.appendChild(text);
+    text.textContent = i;
+    let angleRad = (angle * i * pi) / 180;
+    let textCenterX = yellowR + radius * sin(angleRad);
+    let textCenterY = yellowR - radius * cos(angleRad);
+    text.setAttribute("x", textCenterX);
+    text.setAttribute("y", textCenterY);
+    text.style.fontSize = "50";
+    text.style.fontWeight = "bold";
+    text.style.textAnchor = "middle";
+  }
 }
+numbers();
 
-setInterval(clockRefresh, 1000);
+function lines() {
+  let hourLine = document.createElementNS(svgNS, "line");
+  hourLine.setAttribute("x1", w / 2);
+  hourLine.setAttribute("x2", w / 2);
+  hourLine.setAttribute("y1", w / 2);
+  hourLine.setAttribute("y2", 100);
+  hourLine.setAttribute("stroke", "black");
+  hourLine.setAttribute("stroke-width", 8);
+  hourLine.setAttribute("id", "hours");
+  hourLine.setAttribute("stroke-linecap", "round");
+  svg.appendChild(hourLine);
+  let minLine = document.createElementNS(svgNS, "line");
+  minLine.setAttribute("x1", w / 2);
+  minLine.setAttribute("x2", w / 2);
+  minLine.setAttribute("y1", w / 2);
+  minLine.setAttribute("y2", 60);
+  minLine.setAttribute("stroke", "blue");
+  minLine.setAttribute("stroke-width", 4);
+  minLine.setAttribute("id", "minutes");
+  minLine.setAttribute("stroke-linecap", "round");
+  svg.appendChild(minLine);
+  let secLine = document.createElementNS(svgNS, "line");
+  secLine.setAttribute("x1", w / 2);
+  secLine.setAttribute("x2", w / 2);
+  secLine.setAttribute("y1", w / 2);
+  secLine.setAttribute("y2", 70);
+  secLine.setAttribute("stroke", "red");
+  secLine.setAttribute("stroke-width", 2);
+  secLine.setAttribute("id", "seconds");
+  secLine.setAttribute("stroke-linecap", "round");
+  svg.appendChild(secLine);
+}
+lines();
+
+window.onload = function operation() {
+  setInterval(fSec, 1000);
+
+  function fSec() {
+    let hourArr = document.getElementById("hours");
+    let minArr = document.getElementById("minutes");
+    let secArr = document.getElementById("seconds");
+    let now = new Date();
+    let sec = now.getSeconds() * 6;
+    let min = now.getMinutes() * 6;
+    let hour = (now.getHours() + now.getMinutes() / 60 + now.getSeconds() / 3600) * 30;
+    secArr.setAttribute("transform", "rotate(" + sec + " 300 300)");
+    minArr.setAttribute("transform", "rotate(" + min + " 300 300)");
+    hourArr.setAttribute("transform", "rotate(" + hour + " 300 300)");
+  }
+}
